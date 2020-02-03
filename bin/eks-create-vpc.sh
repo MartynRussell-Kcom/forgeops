7302491effddd11e90d492cb2e36a0d536df8dad
Martyn Russell <martyn.russell@kcom.com>
2020-02-03 14:09:48 +0000

    eks: Add reminder to create hosted zone in Route53 after VPC creation
    Only in cases where we're using a private hosted zone.

--- bin/eks-create-vpc.sh
+++ bin/eks-create-vpc.sh
@@ -25,3 +25,5 @@ echo "Please record the above values for creating your EKS cluster."
 echo "SecurityGroups: This security group allows the EKS cluster to communicate with your worker nodes. Value should be set to EKS_CONTROL_PLANE_SECURITY_GROUP in your eks-env.cfg file."
 echo "VpcId: This is used for creating the subnets in your EC2 account. Value should be set to EKS_VPC_ID in your eks-env.cfg file."
 echo "SubnetIds: This is used to create subnets in your EC2 account where the worker nodes will be launched into. Value should be set to EKS_SUBNETS in your eks-env.cfg file."
+
+echo "NOTE: If using a private zone in Route53, please create it now (if not created yet)."
